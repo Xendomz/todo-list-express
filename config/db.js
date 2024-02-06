768566ec9532b82a5ed298a7c7740fb22226441f
unknown <muhariri.najibullah@gmail.com>
2024-02-06 20:49:25 +0800

    push config

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
 	host: 'sql.freedb.tech',
 	dialect: 'mysql',
-    username: 'freedb_pakisenju',
-    password: 'Fpr&Wt2#Kaau*23',
-    database: 'freedb_sequelize-todo'
+    username: 'freedb_freedb_najib',
+    password: 'n96$$UgXds!8VCc',
+    database: 'freedb_freedb_sequelize-todo'
 });
 
 module.exports = sequelize
